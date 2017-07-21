a09fbadaff7a24f4f4406e389a02c705855203db
Piotr Witkowski <piotrwitkowski@me.com>
2017-07-21 13:30:56 +0200

    working basic version

--- Pace Calculator/ViewController.swift
+++ Pace Calculator/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var txtDistance: UITextField!
+    
+    @IBOutlet var txtTime: UITextField!
+    
+    @IBOutlet var lblPace: UILabel!
+    
+    @IBAction func btnCount(_ sender: Any) {
+        var theDistance:String = txtDistance.text!
+        let theDistanceInt:Int = Int(theDistance)!
+        var theTime:String = txtTime.text!
+        let theTimeInt:Int = Int(theTime)!
+        let pace:Int = theTimeInt / theDistanceInt
+        let paceSeconds:Int = (theTimeInt % theDistanceInt) * 60 / theDistanceInt
+        lblPace.text = "\(pace):\(paceSeconds) min/km"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
